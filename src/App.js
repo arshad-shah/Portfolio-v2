dfb3416ae8c9ee99c5e09cdafd99241a072920da
Arshad Shah <49516613+arshad-shah@users.noreply.github.com>
2021-08-16 13:06:41 +0100

    removed the spacing and characters from the datablock

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ export default function App(props) {
 
         <ScrollToTop {...props}>
           <Fab
-            data-block="back-to-top-button"
+            data-block="backtotopbutton"
             color="secondary"
             size="medium"
             aria-label="scroll back to top"
