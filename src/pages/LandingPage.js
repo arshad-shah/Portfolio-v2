d4a52219d74e1e969e7855aab45babb8a23f1c33
Arshad Shah <49516613+arshad-shah@users.noreply.github.com>
2021-10-08 11:21:59 +0100

    added new email

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -57,7 +57,7 @@ export default function LandingPage() {
           role="button"
           aria-label="link to mail account of Arshad shah"
           data-block="linktoMail"
-          href="mailto:shaharshad1999@outlook.com"
+          href="mailto:info@arshadshah.com"
         >
           <MailIcon fontSize="large" />
         </IconButton>
