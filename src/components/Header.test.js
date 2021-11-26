8e539208554ce0d0aa0b1df059d36630eedcb8b0
Arshad Shah <49516613+arshad-shah@users.noreply.github.com>
2021-11-26 12:29:07 +0000

    added more tests

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,34 +1,43 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import Header from "./Header";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Header from './Header';
 
-describe("Header of the site:", () => {
-  it("should render Header with the creater name in it", () => {
-    render(<Header />);
-    const creator = screen.getByText(/A Shah/i);
-    expect(creator).toBeInTheDocument();
-  });
+describe('Header of the site:', () => {
+	it('should render Header with the creater name in it', () => {
+		render(<Header />);
+		const creator = screen.getByText(/A Shah/i);
+		expect(creator).toBeInTheDocument();
+	});
 
-  it("should render the logo", () => {
-    render(<Header />);
-    const Logo = screen.getByLabelText("Logo");
-    expect(Logo).toBeInTheDocument();
-  });
+	it('should render the logo', () => {
+		render(<Header />);
+		const Logo = screen.getByLabelText('Logo');
+		expect(Logo).toBeInTheDocument();
+	});
 
-  it("should render the Mobile menu", () => {
-    render(<Header isMobile />);
-    const menu = screen.getByLabelText("mobileMenu");
-    expect(menu).toBeInTheDocument();
-  });
+	it('should render the Mobile menu', () => {
+		render(<Header isMobile />);
+		const menu = screen.getByLabelText('mobileMenu');
+		expect(menu).toBeInTheDocument();
+	});
 
-  it("should render the Desktop menu", () => {
-    render(<Header />);
-    const menu = screen.getByLabelText("desktopMenu");
-    expect(menu).toBeInTheDocument();
-  });
+	it('should open mobile menu when menu button is clicked', () => {
+		render(<Header isMobile />);
+		const menu = screen.getByLabelText('mobileMenu');
+		menu.click();
+		expect(screen.getByText(/About me/i)).toBeInTheDocument();
+		expect(screen.getByText(/Projects/i)).toBeInTheDocument();
+		expect(screen.getByText(/Resume/i)).toBeInTheDocument();
+	});
 
-  it("should render Header", () => {
-    const { container } = render(<Header />);
-    expect(container).toMatchSnapshot();
-  });
+	it('should render the Desktop menu', () => {
+		render(<Header />);
+		const menu = screen.getByLabelText('desktopMenu');
+		expect(menu).toBeInTheDocument();
+	});
+
+	it('should render Header', () => {
+		const { container } = render(<Header />);
+		expect(container).toMatchSnapshot();
+	});
 });
