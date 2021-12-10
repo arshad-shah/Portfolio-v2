cf730af9d2ba22a25008fdc4a19f38f9f2bee01d
arshad-shah <shaharshad57@gmail.com>
2021-12-10 16:57:23 +0000

    added test for the app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,40 @@
-import { render, screen } from "@testing-library/react";
+import React from "react";
+import { render } from "@testing-library/react";
 import App from "./App";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import LandingPage from "./pages/LandingPage";
+import Aboutme from "./pages/Aboutme";
+import Projects from "./pages/Projects";
 
-test("renders about me link", () => {
-  render(<App />);
-  const linkElement = screen.getAllByText(/About Me/i);
-  expect(linkElement[0]).toBeInTheDocument();
+describe("the full app:", () => {
+  it("should render the full app", () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should render the Header", () => {
+    const { container } = render(<Header />);
+    expect(container).toBeInTheDocument();
+  });
+
+  it("should render the Landing page", () => {
+    const { container } = render(<LandingPage />);
+    expect(container).toBeInTheDocument();
+  });
+
+  it("should render the About", () => {
+    const { container } = render(<Aboutme />);
+    expect(container).toBeInTheDocument();
+  });
+
+  it("should render the projects", () => {
+    const { container } = render(<Projects />);
+    expect(container).toBeInTheDocument();
+  });
+
+  it("should render the Landing page", () => {
+    const { container } = render(<Footer />);
+    expect(container).toBeInTheDocument();
+  });
 });
