2fcd1d1becd011bb754d4898882a53c4374fdf62
arshad-shah <shaharshad57@gmail.com>
2021-12-10 16:57:24 +0000

    remove redundant import

--- src/components/navigation/Desktop.js
+++ src/components/navigation/Desktop.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Typography } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 import { Link } from "react-scroll";
 import Resume from "../../assets/ArshadResume.pdf";
 import { makeStyles } from "@material-ui/core/styles";
