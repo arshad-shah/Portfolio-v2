b0a57087e91965a4db96bf96ed3ab4c3b48fef20
arshad-shah <shaharshad57@gmail.com>
2021-12-10 17:03:29 +0000

    added more tests

--- src/components/navigation/Mobile.test.js
+++ src/components/navigation/Mobile.test.js
@@ -1,34 +1,38 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import Mobile from "./Mobile";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Mobile from './Mobile';
 
-describe("Mobile of the site:", () => {
-  it("should render Mobile menu with the opening hamburger button", () => {
-    render(<Mobile />);
-    const menuIcon = screen.getByLabelText("menu toggle");
-    expect(menuIcon).toBeInTheDocument();
-  });
+const closeMobileMenu = jest.fn();
+const handleMenu = jest.fn();
+describe('Mobile of the site:', () => {
+	it('should render Mobile menu with the opening hamburger button', () => {
+		render(<Mobile click={false} closeMobileMenu={closeMobileMenu} handleMenu={handleMenu} />);
+		const menuIcon = screen.getByLabelText('menu toggle');
+		expect(menuIcon).toBeInTheDocument();
+	});
 
-  it("should render Mobile menu with the About me link", () => {
-    render(<Mobile />);
-    const aboutme = screen.getByText("About Me");
-    expect(aboutme).toBeInTheDocument();
-  });
+	it('should render Mobile menu with the About me link', () => {
+		render(<Mobile click={false} closeMobileMenu={closeMobileMenu} handleMenu={handleMenu} />);
+		const aboutme = screen.getByText('About Me');
+		expect(aboutme).toBeInTheDocument();
+	});
 
-  it("should render Mobile menu with the Projects link", () => {
-    render(<Mobile />);
-    const Projects = screen.getByText("Projects");
-    expect(Projects).toBeInTheDocument();
-  });
+	it('should render Mobile menu with the Projects link', () => {
+		render(<Mobile click={false} closeMobileMenu={closeMobileMenu} handleMenu={handleMenu} />);
+		const Projects = screen.getByText('Projects');
+		expect(Projects).toBeInTheDocument();
+	});
 
-  it("should render Mobile menu with the Resume link", () => {
-    render(<Mobile />);
-    const Resume = screen.getByText("Resume");
-    expect(Resume).toBeInTheDocument();
-  });
+	it('should render Mobile menu with the Resume link', () => {
+		render(<Mobile click={false} closeMobileMenu={closeMobileMenu} handleMenu={handleMenu} />);
+		const Resume = screen.getByText('Resume');
+		expect(Resume).toBeInTheDocument();
+	});
 
-  it("should render Mobile", () => {
-    const { container } = render(<Mobile />);
-    expect(container).toMatchSnapshot();
-  });
+	it('should render Mobile', () => {
+		const { container } = render(
+			<Mobile click={false} closeMobileMenu={closeMobileMenu} handleMenu={handleMenu} />
+		);
+		expect(container).toMatchSnapshot();
+	});
 });
