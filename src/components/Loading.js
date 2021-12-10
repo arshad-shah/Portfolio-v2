5219cb7ed1c8ee09ecc02fcb90f9d160739ed3e7
arshad-shah <shaharshad57@gmail.com>
2021-12-10 16:57:25 +0000

    Optimzed site

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { CircularProgress, Typography } from "@material-ui/core";
-import Logo from "../assets/logo.svg";
 import useStyles from "./Loading.styles";
 
 export default function Loading() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <img src={Logo} alt="Logo" width="100" height="100" />
       <Typography className={classes.name} variant="h4">
-        A SHAH
+        Loading.....
       </Typography>
       <CircularProgress color="secondary" />
     </div>
