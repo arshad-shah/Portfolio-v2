66625c0134fe315f3c3a1ef6ba8d46fb62d80411
arshad-shah <shaharshad57@gmail.com>
2021-12-12 07:15:49 +0000

    linting

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -1,40 +1,40 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "../App";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import LandingPage from "../pages/LandingPage";
-import Aboutme from "../pages/Aboutme";
-import Projects from "../pages/Projects";
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from '../App';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import LandingPage from '../pages/LandingPage';
+import Aboutme from '../pages/Aboutme';
+import Projects from '../pages/Projects';
 
-describe("the full app:", () => {
-  it("should render the full app", () => {
-    const { container } = render(<App />);
-    expect(container).toMatchSnapshot();
-  });
+describe('the full app:', () => {
+	it('should render the full app', () => {
+		const { container } = render(<App />);
+		expect(container).toMatchSnapshot();
+	});
 
-  it("should render the Header", () => {
-    const { container } = render(<Header />);
-    expect(container).toBeInTheDocument();
-  });
+	it('should render the Header', () => {
+		const { container } = render(<Header />);
+		expect(container).toBeInTheDocument();
+	});
 
-  it("should render the Landing page", () => {
-    const { container } = render(<LandingPage />);
-    expect(container).toBeInTheDocument();
-  });
+	it('should render the Landing page', () => {
+		const { container } = render(<LandingPage />);
+		expect(container).toBeInTheDocument();
+	});
 
-  it("should render the About", () => {
-    const { container } = render(<Aboutme />);
-    expect(container).toBeInTheDocument();
-  });
+	it('should render the About', () => {
+		const { container } = render(<Aboutme />);
+		expect(container).toBeInTheDocument();
+	});
 
-  it("should render the projects", () => {
-    const { container } = render(<Projects />);
-    expect(container).toBeInTheDocument();
-  });
+	it('should render the projects', () => {
+		const { container } = render(<Projects />);
+		expect(container).toBeInTheDocument();
+	});
 
-  it("should render the Footer", () => {
-    const { container } = render(<Footer />);
-    expect(container).toBeInTheDocument();
-  });
+	it('should render the Footer', () => {
+		const { container } = render(<Footer />);
+		expect(container).toBeInTheDocument();
+	});
 });
