16e9e1b76d9b2587afa935dd8a3292aa251d0e03
Arshad Shah <49516613+arshad-shah@users.noreply.github.com>
2021-12-16 11:54:37 +0000

    add hacker rank link and update snapshots (#6)

--- src/_tests_/LandingPage.test.js
+++ src/_tests_/LandingPage.test.js
@@ -1,41 +1,46 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import LandingPage from '../pages/LandingPage';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import LandingPage from "../pages/LandingPage";
 
-describe('LandingPage of the site:', () => {
-	it('should render LandingPage with all the information in it', () => {
-		const { container } = render(<LandingPage />);
-		expect(container).toMatchSnapshot();
-	});
+describe("LandingPage of the site:", () => {
+  it("should render LandingPage with all the information in it", () => {
+    const { container } = render(<LandingPage />);
+    expect(container).toMatchSnapshot();
+  });
 
-	it('should render the Hero text', () => {
-		render(<LandingPage />);
-		const nameOfArshad = screen.getByText('Arshad Shah!');
-		expect(nameOfArshad).toBeInTheDocument();
-	});
+  it("should render the Hero text", () => {
+    render(<LandingPage />);
+    const nameOfArshad = screen.getByText("Arshad Shah!");
+    expect(nameOfArshad).toBeInTheDocument();
+  });
 
-	it('should render the social links', () => {
-		render(<LandingPage />);
-		const githubIcon = screen.getByRole('button', {
-			name: /link to Github account of Arshad shah/i,
-		});
-		const linkedInIcon = screen.getByRole('button', {
-			name: /link to Linkedin account of Arshad shah/i,
-		});
-		const mailIcon = screen.getByRole('button', {
-			name: /link to mail account of Arshad shah/i,
-		});
+  it("should render the social account links", () => {
+    render(<LandingPage />);
+    const githubIcon = screen.getByRole("button", {
+      name: /link to Github account of Arshad shah/i,
+    });
+    const linkedInIcon = screen.getByRole("button", {
+      name: /link to Linkedin account of Arshad shah/i,
+    });
 
-		expect(githubIcon).toBeInTheDocument();
-		expect(linkedInIcon).toBeInTheDocument();
-		expect(mailIcon).toBeInTheDocument();
-	});
+    const hackerRankIcon = screen.getByRole("button", {
+      name: /link to Hacker Rank account of Arshad shah/i,
+    });
+    const mailIcon = screen.getByRole("button", {
+      name: /link to mail account of Arshad shah/i,
+    });
 
-	it('should render the scroll down button', () => {
-		render(<LandingPage />);
-		const scrollDownButton = screen.getByRole('button', {
-			name: /scroll down button/i,
-		});
-		expect(scrollDownButton).toBeInTheDocument();
-	});
+    expect(githubIcon).toBeInTheDocument();
+    expect(linkedInIcon).toBeInTheDocument();
+    expect(hackerRankIcon).toBeInTheDocument();
+    expect(mailIcon).toBeInTheDocument();
+  });
+
+  it("should render the scroll down button", () => {
+    render(<LandingPage />);
+    const scrollDownButton = screen.getByRole("button", {
+      name: /scroll down button/i,
+    });
+    expect(scrollDownButton).toBeInTheDocument();
+  });
 });
