ebba2931ff1f03de32ff413c52a365ac8e3c4dc3
arshad-shah <shaharshad57@gmail.com>
2021-12-23 14:07:24 +0000

    removed env files

--- src/index.js
+++ src/index.js
@@ -6,14 +6,11 @@ import App from './App';
 
 import TagManager from 'react-gtm-module';
 
-const env = process.env.NODE_ENV;
-if (env === 'production') {
-	const tagManagerArgs = {
-		gtmId: process.env.REACT_APP_GOOGLE_CONTAINER_ID,
-	};
+const tagManagerArgs = {
+	gtmId: 'GTM-NBW8RKR',
+};
 
-	TagManager.initialize(tagManagerArgs);
-	ReactGA.initialize(process.env.REACT_APP_GOOGLE_TRACKING_ID);
-	ReactGA.pageview(window.location.pathname + window.location.search);
-}
+TagManager.initialize(tagManagerArgs);
+ReactGA.initialize('UA-205064366-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 ReactDOM.render(<App />, document.getElementById('root'));
