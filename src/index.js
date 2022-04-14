3956142fa54299d168c0658b4db9f7068e6f923b
Arshad Shah <49516613+arshad-shah@users.noreply.github.com>
2022-04-14 19:18:22 +0100

    update packages and snapshots

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import ReactGA from "react-ga";
 import App from "./App";
 
 import TagManager from "react-gtm-module";
+
+import { createRoot } from "react-dom/client";
 
 const tagManagerArgs = {
   gtmId: "GTM-NBW8RKR",
@@ -13,4 +14,7 @@ const tagManagerArgs = {
 TagManager.initialize(tagManagerArgs);
 ReactGA.initialize("UA-205064366-1");
 ReactGA.pageview(window.location.pathname + window.location.search);
-ReactDOM.render(<App />, document.getElementById("root"));
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
